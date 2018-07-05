4eee57d92b3dae9107dd048aedeee3b7e58d2146
WeRockStar <kotchaphan.m@gmail.com>
2018-07-05 07:33:42 +0700

    Rename mock to stub

--- src/test/kotlin/DateTimerTest.kt
+++ src/test/kotlin/DateTimerTest.kt
@@ -7,7 +7,7 @@ class DateTimerTest {
 
     @Test
     fun `when call getNowString() should see current datetime`() {
-        val dateTimeManager = MockDateTimeService()
+        val dateTimeManager = StubDateTimeService()
         val greetingDateTime = DateTimer(dateTimeManager);
 
         val expected = "2018/07/04_18:00:40.000"
@@ -15,7 +15,7 @@ class DateTimerTest {
         assertEquals(expected, greetingDateTime.getNowString())
     }
 
-    class MockDateTimeService : DateTimeService() {
+    class StubDateTimeService : DateTimeService() {
         override fun now() = "2018/07/04_18:00:40.000"
     }
 }

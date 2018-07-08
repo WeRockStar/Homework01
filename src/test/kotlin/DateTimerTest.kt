9d7346aec9454fef0e5184b1f0b529547f26390e
WeRockStar <kotchaphan.m@gmail.com>
2018-07-08 20:06:22 +0700

    Rename variable meaningful

--- src/test/kotlin/DateTimerTest.kt
+++ src/test/kotlin/DateTimerTest.kt
@@ -1,5 +1,4 @@
 import datetime.DateTimeService
-import datetime.DateTimer
 import datetime.ITimeProvider
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -10,11 +9,11 @@ class DateTimerTest {
     @Test
     fun `when call getNowString() should see current datetime`() {
         val dateTimeManager = StubTimeProvider()
-        val greetingDateTime = DateTimeService(dateTimeManager)
+        val dateTimeService = DateTimeService(dateTimeManager)
 
         val expected = "2018/07/04_18:00:40.000"
 
-        assertEquals(expected, greetingDateTime.now())
+        assertEquals(expected, dateTimeService.now())
     }
 
     class StubTimeProvider : ITimeProvider {
